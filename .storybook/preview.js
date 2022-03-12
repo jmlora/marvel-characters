a22483434d9396f62e3331df336a03135b639d4e
Juan M. Lora <juanmanuel.loram@gmail.com>
2022-03-12 14:48:17 +0100

    fix(global-styles): fix reset styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
-import '!style-loader!css-loader!sass-loader!../src/assets/styles/index.scss'
 import '!style-loader!css-loader!sass-loader!../src/assets/styles/reset.scss'
+import '!style-loader!css-loader!sass-loader!../src/assets/styles/index.scss'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
