a7d843621cc1aadb07274ba0cee3427d1edc557c
Juan M. Lora <juanmanuel.loram@gmail.com>
2022-03-13 13:31:33 +0100

    feat(app): add search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.scss'
+import { SearchInput } from './components/inputs/SearchInput'
+import { CardList } from './components/cards/CardList'
+import { CardProps } from './components/cards/CardList/CardList'
 
 function App() {
+  const cards:Array<CardProps> = []
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="container"></div>
+      <div className="container">
+        <SearchInput />
+        <CardList cards={cards} />
+      </div>
     </div>
   );
 }
